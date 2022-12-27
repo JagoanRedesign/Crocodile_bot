e0e5dd64289e7fbfd11722a89488ac2dec83ed1c
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-27 20:03:24 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 PROXY_URL = 'https://54.37.131.45:3128'
 REQUEST_KWARGS = {'proxy_url': 'https://54.37.131.45:3128'}
-TOKEN = "1221980714:AAHuDcBOFq5ITPnG_kPQnQ76RxUfZStkJB4"
+TOKEN = "5987246649:AAEY6PbuAw6_Y-OpbN2KfWahDQnGuJCcLxE"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
